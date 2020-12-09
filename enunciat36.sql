e40a5a093816498171c4613a2955b46db5bb6e4b
POPBOBPOP <xavierbermudez2000@ginebro.cat>
2020-12-09 19:16:10 +0100

    Afegit el archiu sql

---
+++ enunciat36.sql
@@ -0,0 +1,7 @@
+mysql> SELECT titol_peli,recaudacio_peli  FROM PELLICULES ORDER BY recaudacio_peli ASC LIMIT 1;
++------------+-----------------+
+| titol_peli | recaudacio_peli |
++------------+-----------------+
+| MABOROSHI  |          144025 |
++------------+-----------------+
+1 row in set (0.00 sec)
